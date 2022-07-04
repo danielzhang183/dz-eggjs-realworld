cdb5803f3a254739f1090af023dbd1295a57509e
danielzhang183 <danielzhang182@gmail.com>
2022-07-04 08:31:56 +0800

    chore: add error handler middleware

--- config/config.default.js
+++ config/config.default.js
@@ -16,7 +16,9 @@ module.exports = appInfo => {
   config.keys = appInfo.name + '_1656845345478_7597';
 
   // add your middleware config here
-  config.middleware = [];
+  config.middleware = [
+    'errorHandler',
+  ];
 
   // add your user config here
   const userConfig = {
